afcf56ea99a6df8d710cf89593337838034d3fca
Veronica Blaha <veronica.blaha@gmail.com>
2018-11-09 21:44:28 -0500

    initial refactor

--- pages/index.js
+++ pages/index.js
@@ -2,60 +2,56 @@ import Head from '../components/head';
 import EvalText from '../components/evaltext';
 import Header from '../components/header';
 import Counters from '../components/counters';
-import {femaleList, maleList, neutralList} from '../components/words';
+import { femaleList, maleList, neutralList } from '../components/words';
 import { countInstances } from '../logic/count_func';
 
-  class Index extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        textInput: '',
-        maleCount: 0,
-        femaleCount: 0,
-        neutralCount: 0
-      };
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.analyzeText = this.analyzeText.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({
-        textInput: event.target.value
-      });
-    }
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textInput: '',
+      maleCount: 0,
+      femaleCount: 0,
+      neutralCount: 0
+    };
 
-    analyzeText(event) {
-      event.preventDefault();
-      this.setState({
-        maleCount: countInstances(maleList, this.state.textInput.split(" ")),
-        femaleCount: countInstances(femaleList, this.state.textInput.split(" ")),
-        neutralCount: countInstances(neutralList, this.state.textInput.split(" "))
-      });
-      console.log(this.state)
-    }
-  
-    render() {
+    this.handleChange = this.handleChange.bind(this);
+    this.analyzeText = this.analyzeText.bind(this);
+  }
 
-      const formattedText = 
-        this.state.textInput.length > 0 ? this.state.textInput.split(" ").map(word => <EvalText word={word} />) : 'Waiting for your input.'
+  handleChange(event) {
+    this.setState({
+      textInput: event.target.value
+    });
+  }
 
-      return (
-        <div>
-          <Head />
-          <style jsx>{`
+  analyzeText(event) {
+    event.preventDefault();
+    this.setState({
+      maleCount: countInstances(maleList, this.state.textInput.split(" ")),
+      femaleCount: countInstances(femaleList, this.state.textInput.split(" ")),
+      neutralCount: countInstances(neutralList, this.state.textInput.split(" "))
+    });
+    console.log(this.state)
+  }
+
+  render() {
+
+    const formattedText =
+      this.state.textInput.length > 0 ? this.state.textInput.split(" ").map(word => <EvalText word={word} />) : 'Waiting for your input.'
+
+    return (
+      <div>
+        <Head />
+        <style jsx>{`
             @import url('https://fonts.googleapis.com/css?family=Kumar+One+Outline');
             
-            .body {
-              display: flex;
-              flex-direction: column;
-            }
 
             .main-content-wrapper {
-              display: flex;
-              justify-content: center;
+              display: grid;
+              grid-template-columns: 1fr 1fr;
+              // grid-template-rows: 1fr 1fr;
             }
-
             .form-area {
               margin-right: 5px;
             }
@@ -64,35 +60,58 @@ import { countInstances } from '../logic/count_func';
               margin: 6px;
               background: black;
             }
+            .text-area {
+              // background-color: #eee;
+            }
             .counter-box {
-              margin-top: 22em;
               padding: 0px 2px 0px 1px;
+              // background-color: #eee;
+            }
+            button {
+              padding: 10px 20px;
+              background-color: transparent;
               border-radius: 10px;
-              border-width: 5px;
-              border-style: outset;
-              border-color: red;
+            }
+            textarea {
+              resize: none;
+            }
+            @media only screen and (max-width: 600px) {
+              .main-content-wrapper {
+                display: grid;
+                grid-template-areas: "1fr 1fr"
+                                     "1fr"
+                                     "1fr";
+                grid-gap
+                // grid-template-columns: 1fr;
+                // grid-template-rows: 1fr 1fr;
+              }
+              .analyze, .counter-box  {
+                order: -1;
+                padding-bottom: 2em;
+              }              
             }
           `}</style>
-          <Header />
-          <div className="main-content-wrapper">
-            <div className="form-area">
-              <form>            
-                  <textarea rows="30" cols="60" value={this.state.value} onChange={this.handleChange} id="input-area" />
-              </form>
-              <button onClick={(event) => this.analyzeText(event)}>Analyze Text</button>
-            </div>
-            <div className="divider"></div>
-            <div className="text-area">
-              {formattedText}
-              <div className="counter-box">
-                <Counters 
-                  femaleNum={this.state.femaleCount} maleNum={this.state.maleCount} neutralNum={this.state.neutralCount} 
-                />
-              </div>
-            </div>
+        <Header />
+        <div className="main-content-wrapper">
+          <div className="form-area">
+            <form>
+              <textarea rows="30" cols="60" value={this.state.value} onChange={this.handleChange} id="input-area" placeholder="Start typing here"/>
+            </form>
+          </div>
+          <div className="text-area">
+            {formattedText}
+          </div>
+          <div className="analyze">
+            <button onClick={(event) => this.analyzeText(event)}>Analyze Text</button>
+          </div>
+          <div className="counter-box">
+            <Counters
+              femaleNum={this.state.femaleCount} maleNum={this.state.maleCount} neutralNum={this.state.neutralCount}
+            />
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
 export default Index;
